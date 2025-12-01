5cded28c92bc184051339485d28136d969a7a291
AlejNCs <alejnc0@gmail.com>
2025-12-01 11:28:10 -0600

    Correción de la  ruta del Dockerfile en build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 # Construir la imagen Docker etiquetada como 'mi-calculadora'
-docker build -t mi-calculadora:latest .
+#docker build -t mi-calculadora:latest .
+#!/bin/bash
+set -e
+
+# Usar el Dockerfile que está en src/
+docker build -t calculadora -f src/Dockerfile .
